8d2924aebdcbbee62ccd5ecf18d85609867e7dde
Brendan O'Dea <brendan.ode4@gmail.com>
2018-05-15 22:10:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
 function logDriverNames(drivers){
-  function printName (el, i, arr) {return console.log(el.name)}
-  return drivers.foreach(printName)
+  function printName (el, i, arr) {console.log(el.name)}
+  return drivers.forEach(printName)
 }
